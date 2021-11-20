8ba1f4545c4857cd7ee9df975a5047d299f00782
Whice <And-not@mail.ru>
2021-11-20 12:31:14 +0300

    Добавлен минимальный интерфейс

--- WordMemorizationAssistant/Form1.Designer.cs
+++ WordMemorizationAssistant/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WordMemorizationAssistant
 {
-    partial class Form1
+    partial class FormWordMemorizationAssistant
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,191 @@ namespace WordMemorizationAssistant
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxUserAnswer = new System.Windows.Forms.TextBox();
+            this.labelWordForTranslate = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageLearnWords = new System.Windows.Forms.TabPage();
+            this.tabPageAddWords = new System.Windows.Forms.TabPage();
+            this.labelUserAnswer = new System.Windows.Forms.Label();
+            this.labelRightAnswer = new System.Windows.Forms.Label();
+            this.buttonCheckNexWord = new System.Windows.Forms.Button();
+            this.labelInputEnglishWord = new System.Windows.Forms.Label();
+            this.textBoxInputEnglishWord = new System.Windows.Forms.TextBox();
+            this.textBoxInputTranslateWord = new System.Windows.Forms.TextBox();
+            this.labelInputTranslateWord = new System.Windows.Forms.Label();
+            this.buttonAddWord = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPageLearnWords.SuspendLayout();
+            this.tabPageAddWords.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUserAnswer
+            // 
+            this.textBoxUserAnswer.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUserAnswer.Location = new System.Drawing.Point(181, 94);
+            this.textBoxUserAnswer.Name = "textBoxUserAnswer";
+            this.textBoxUserAnswer.Size = new System.Drawing.Size(237, 46);
+            this.textBoxUserAnswer.TabIndex = 0;
+            // 
+            // labelWordForTranslate
+            // 
+            this.labelWordForTranslate.AutoSize = true;
+            this.labelWordForTranslate.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelWordForTranslate.Location = new System.Drawing.Point(8, 14);
+            this.labelWordForTranslate.Name = "labelWordForTranslate";
+            this.labelWordForTranslate.Size = new System.Drawing.Size(240, 40);
+            this.labelWordForTranslate.TabIndex = 1;
+            this.labelWordForTranslate.Text = "WordForTranslate";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageLearnWords);
+            this.tabControl1.Controls.Add(this.tabPageAddWords);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(520, 450);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPageLearnWords
+            // 
+            this.tabPageLearnWords.Controls.Add(this.buttonCheckNexWord);
+            this.tabPageLearnWords.Controls.Add(this.labelRightAnswer);
+            this.tabPageLearnWords.Controls.Add(this.labelUserAnswer);
+            this.tabPageLearnWords.Controls.Add(this.labelWordForTranslate);
+            this.tabPageLearnWords.Controls.Add(this.textBoxUserAnswer);
+            this.tabPageLearnWords.Location = new System.Drawing.Point(4, 24);
+            this.tabPageLearnWords.Name = "tabPageLearnWords";
+            this.tabPageLearnWords.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLearnWords.Size = new System.Drawing.Size(792, 422);
+            this.tabPageLearnWords.TabIndex = 0;
+            this.tabPageLearnWords.Text = "tabPageLearnWords";
+            this.tabPageLearnWords.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAddWords
+            // 
+            this.tabPageAddWords.Controls.Add(this.buttonAddWord);
+            this.tabPageAddWords.Controls.Add(this.textBoxInputTranslateWord);
+            this.tabPageAddWords.Controls.Add(this.labelInputTranslateWord);
+            this.tabPageAddWords.Controls.Add(this.textBoxInputEnglishWord);
+            this.tabPageAddWords.Controls.Add(this.labelInputEnglishWord);
+            this.tabPageAddWords.Location = new System.Drawing.Point(4, 24);
+            this.tabPageAddWords.Name = "tabPageAddWords";
+            this.tabPageAddWords.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAddWords.Size = new System.Drawing.Size(512, 422);
+            this.tabPageAddWords.TabIndex = 1;
+            this.tabPageAddWords.Text = "tabPageAddWords";
+            this.tabPageAddWords.UseVisualStyleBackColor = true;
+            // 
+            // labelUserAnswer
+            // 
+            this.labelUserAnswer.AutoSize = true;
+            this.labelUserAnswer.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelUserAnswer.Location = new System.Drawing.Point(8, 94);
+            this.labelUserAnswer.Name = "labelUserAnswer";
+            this.labelUserAnswer.Size = new System.Drawing.Size(167, 40);
+            this.labelUserAnswer.TabIndex = 2;
+            this.labelUserAnswer.Text = "UserAnswer";
+            // 
+            // labelRightAnswer
+            // 
+            this.labelRightAnswer.AutoSize = true;
+            this.labelRightAnswer.Font = new System.Drawing.Font("Nirmala UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelRightAnswer.Location = new System.Drawing.Point(8, 154);
+            this.labelRightAnswer.Name = "labelRightAnswer";
+            this.labelRightAnswer.Size = new System.Drawing.Size(177, 40);
+            this.labelRightAnswer.TabIndex = 4;
+            this.labelRightAnswer.Text = "RightAnswer";
+            // 
+            // buttonCheckNexWord
+            // 
+            this.buttonCheckNexWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCheckNexWord.Location = new System.Drawing.Point(8, 214);
+            this.buttonCheckNexWord.Name = "buttonCheckNexWord";
+            this.buttonCheckNexWord.Size = new System.Drawing.Size(325, 102);
+            this.buttonCheckNexWord.TabIndex = 5;
+            this.buttonCheckNexWord.Text = "buttonCheckNexWord";
+            this.buttonCheckNexWord.UseVisualStyleBackColor = true;
+            // 
+            // labelInputEnglishWord
+            // 
+            this.labelInputEnglishWord.AutoSize = true;
+            this.labelInputEnglishWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelInputEnglishWord.Location = new System.Drawing.Point(8, 14);
+            this.labelInputEnglishWord.Name = "labelInputEnglishWord";
+            this.labelInputEnglishWord.Size = new System.Drawing.Size(305, 40);
+            this.labelInputEnglishWord.TabIndex = 0;
+            this.labelInputEnglishWord.Text = "labelInputEnglishWord";
+            // 
+            // textBoxInputEnglishWord
+            // 
+            this.textBoxInputEnglishWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxInputEnglishWord.Location = new System.Drawing.Point(8, 57);
+            this.textBoxInputEnglishWord.Name = "textBoxInputEnglishWord";
+            this.textBoxInputEnglishWord.Size = new System.Drawing.Size(404, 46);
+            this.textBoxInputEnglishWord.TabIndex = 1;
+            // 
+            // textBoxInputTranslateWord
+            // 
+            this.textBoxInputTranslateWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxInputTranslateWord.Location = new System.Drawing.Point(8, 169);
+            this.textBoxInputTranslateWord.Name = "textBoxInputTranslateWord";
+            this.textBoxInputTranslateWord.Size = new System.Drawing.Size(404, 46);
+            this.textBoxInputTranslateWord.TabIndex = 3;
+            // 
+            // labelInputTranslateWord
+            // 
+            this.labelInputTranslateWord.AutoSize = true;
+            this.labelInputTranslateWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelInputTranslateWord.Location = new System.Drawing.Point(8, 126);
+            this.labelInputTranslateWord.Name = "labelInputTranslateWord";
+            this.labelInputTranslateWord.Size = new System.Drawing.Size(327, 40);
+            this.labelInputTranslateWord.TabIndex = 2;
+            this.labelInputTranslateWord.Text = "labelInputTranslateWord";
+            // 
+            // buttonAddWord
+            // 
+            this.buttonAddWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonAddWord.Location = new System.Drawing.Point(8, 294);
+            this.buttonAddWord.Name = "buttonAddWord";
+            this.buttonAddWord.Size = new System.Drawing.Size(325, 102);
+            this.buttonAddWord.TabIndex = 6;
+            this.buttonAddWord.Text = "buttonAddWord";
+            this.buttonAddWord.UseVisualStyleBackColor = true;
+            // 
+            // FormWordMemorizationAssistant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(520, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FormWordMemorizationAssistant";
+            this.Text = "Word memorization assistant";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageLearnWords.ResumeLayout(false);
+            this.tabPageLearnWords.PerformLayout();
+            this.tabPageAddWords.ResumeLayout(false);
+            this.tabPageAddWords.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserAnswer;
+        private System.Windows.Forms.Label labelWordForTranslate;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageLearnWords;
+        private System.Windows.Forms.Label labelRightAnswer;
+        private System.Windows.Forms.Label labelUserAnswer;
+        private System.Windows.Forms.TabPage tabPageAddWords;
+        private System.Windows.Forms.Button buttonCheckNexWord;
+        private System.Windows.Forms.Button buttonAddWord;
+        private System.Windows.Forms.TextBox textBoxInputTranslateWord;
+        private System.Windows.Forms.Label labelInputTranslateWord;
+        private System.Windows.Forms.TextBox textBoxInputEnglishWord;
+        private System.Windows.Forms.Label labelInputEnglishWord;
     }
 }
 
--- WordMemorizationAssistant/Form1.cs
+++ WordMemorizationAssistant/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WordMemorizationAssistant
 {
-    public partial class Form1 : Form
+    public partial class FormWordMemorizationAssistant : Form
     {
-        public Form1()
+        public FormWordMemorizationAssistant()
         {
             InitializeComponent();
         }
